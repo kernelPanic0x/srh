5fed9c8e7a67211288e0899fa176fe1646a51372
Elias Dalbeck <eliasdalbeck42@gmail.com>
2025-12-07 13:14:18 +0100

    authorization via pub key

--- srh.sh
+++ srh.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-set -e 
+set -euo pipefail
 
 # 1. Define the specific version of magic-wormhole-rs
 WORMHOLE_URL="https://github.com/magic-wormhole/magic-wormhole.rs/releases/download/0.7.6/magic-wormhole-cli-x86_64-unknown-linux-gnu.tgz"
 WORK_DIR=$(mktemp -d)
+SSH_PUB_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDobwWOoPssm0t4leNnOw/uDyRD83vKgSZTw68AiKquX elias@archlinux"
 export WORMHOLE_RELAY_URL=tcp://nbg.ell.dns64.de:4001
+
+cleanup() {
+    set +e
+
+    echo "[+] Removing SSH access again..."
+    grep -v -F -x "$SSH_PUB_KEY" ~/.ssh/authorized_keys > ~/.ssh/tmp
+    mv ~/.ssh/tmp ~/.ssh/authorized_keys
+
+    echo "[+] Stopping wormhole"
+    kill -SIGINT $(pgrep wormhole-rs)
+}
+
+trap cleanup EXIT
+trap cleanup ERR
 
 echo "---------------------------------------------------"
 echo "               Shell Remote Help"
+echo ""
 echo "             Authors: Elias Dalbeck"
 echo "---------------------------------------------------"
 
 # 2. Prepare the directory and download
 mkdir -p "$WORK_DIR"
 cd "$WORK_DIR"
+echo "[i] Using temp directory: $WORK_DIR"
 
 echo "[+] Checking if screen is installed..."
 if ! command -v screen >/dev/null 2>&1; then
@@ -40,33 +57,27 @@ tar -xzf magic-wormhole-cli-x86_64-unknown-linux-gnu.tgz
 BIN_PATH=$(find . -type f -name "wormhole-rs" -o -name "wormhole" | head -n 1)
 chmod +x "$BIN_PATH"
 
-# 4. Setup SSH Server
+echo "[+] Authorizing SSH..."
+echo $SSH_PUB_KEY >> ~/.ssh/authorized_keys
+
 echo "[+] Starting SSH Server..."
 sudo systemctl start sshd
 
-if [ -z "$SKIP_PASSWD" ]; then
-    echo "[+] Please change your user password for temp access:"
-    sudo passwd $(whoami)
-else
-    echo "[+] Skipping passwd."
-fi
-
-
 echo "[+] Starting Wormhole Tunnel..."
-echo "    Generating code... please wait."
-
-# Start wormhole in background, log output to file for display
 "$BIN_PATH" forward serve 127.0.0.1:22 > "$WORK_DIR/wormhole.log" 2>&1 &
 WH_PID=$!
+
+echo "    Generating code... please wait."
 
 # Wait loop to display the code once it appears in the log
 COUNT=0
 while [ $COUNT -lt 10 ]; do
-    if grep -q "code is:" "$WORK_DIR/wormhole.log" || true; then
+    if grep -q "code is:" "$WORK_DIR/wormhole.log"; then
         break
     fi
+    
     sleep 1
-    ((COUNT++))
+    COUNT=$((COUNT++))
 done
 
 echo ""
@@ -75,8 +86,9 @@ echo "        YOUR SHARED SESSION CODE IS BELOW"
 echo "###################################################"
 echo ""
 # Display the code cleanly
-grep "code is:" "$WORK_DIR/wormhole.log" -A 1
-
+grep "code is:" "$WORK_DIR/wormhole.log" -A 1 
+echo ""
+echo ">>> Your Username: $(whoami) <<<"
 read -p "Press [Enter] when ready..."
 
 echo "[+] Starting shared screen session..."
