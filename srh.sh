975a7b4c189a217f1e68db08125ea9eb57b777f8
Elias Dalbeck <eliasdalbeck42@gmail.com>
2025-12-09 08:16:10 +0100

    try force OSC52 for clonezilla

--- srh.sh
+++ srh.sh
@@ -63,9 +63,10 @@ mkdir -p "$WORK_DIR"
 cd "$WORK_DIR"
 cat << 'EOF' > "$WORK_DIR/.screenrc"
 truecolor on
-
 hardstatus alwaysfirstline
 hardstatus string '%{= 0;5}%= Shared Shell Session %{= 0;5}%= %c'
+term xterm-256color
+termcapinfo * "Ms=\E]52;%p1%s;%p2%s\007"
 EOF
 echo "${GREEN}Ok${RESET}"
 
